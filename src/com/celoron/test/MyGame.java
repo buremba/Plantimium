45c19bf627af710275d64135888f47594ab399f1
Celaleddin Subasi <celoron@gmail.com>
2012-03-28 00:15:51 +0300

    quick fix

--- src/com/celoron/test/MyGame.java
+++ src/com/celoron/test/MyGame.java
@@ -1,6 +1,7 @@
 package com.celoron.test;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.backends.jogl.JoglApplication;
 import com.badlogic.gdx.graphics.Texture;
 import com.celoron.engine.Entity;
 import com.celoron.engine.Game;
@@ -22,5 +23,9 @@ public class MyGame extends Game {
 		 * There is nothing i haveto think. after all components doing all work :D
 		 * */
 	}
+	
+    public static void main(String[] args) {
+        new JoglApplication(new MyGame(), "My Game", 480, 320, false);
+    }
 	
 }
